35ef412ecc8602e63e4f5aaf0327a876bffb28f1
Zoran Zaric <zz@zoranzaric.de>
2013-02-14 03:14:29 +0100

    Add layout

--- gal/settings.py
+++ gal/settings.py
@@ -66,9 +66,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(base_directory, '../static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -105,9 +103,7 @@ ROOT_URLCONF = 'gal.urls'
 WSGI_APPLICATION = 'gal.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(base_directory, '../templates'),
 )
 
 INSTALLED_APPS = (
