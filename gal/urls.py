1eb8a90bad7677e1aa4ec703d43c5f5369c1be99
Zoran Zaric <zz@zoranzaric.de>
2013-02-14 03:28:32 +0100

    Add index

--- gal/urls.py
+++ gal/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'gal.views.home', name='home'),
     # url(r'^gal/', include('gal.foo.urls')),
 
-    url(r'^image/(.+)$', 'gal.views.view_image'),
+    url(r'^$', 'gal.views.index'),
+    url(r'^image/(.+)$', 'gal.views.view_image', name='view_image'),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
--- gal/views.py
+++ gal/views.py
@@ -2,10 +2,17 @@ import mimetypes
 import os
 
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from django.core.servers.basehttp import FileWrapper
 from gal.models import Image
 from gal import settings
+
+
+def index(request):
+    images = [image.filename for image in Image.objects.all()]
+    return render_to_response('index.html', {'title': 'index',
+                                             'images': images})
+
 
 def view_image(request, filename):
     image = get_object_or_404(Image, filename=filename)
