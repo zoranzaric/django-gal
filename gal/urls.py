6458ae2eaa4864689227bffe5046ec2856715b43
Zoran Zaric <zz@zoranzaric.de>
2013-02-17 04:27:41 +0100

    Improve url regexes

--- gal/urls.py
+++ gal/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'gal.views.home'),
-    url(r'^(?P<gallery>.+)/download$', 'gal.views.download_all'),
-    url(r'^(?P<gallery>.+)/image/(?P<filename>.+)$', 'gal.views.view_image', name='view_image'),
-    url(r'^(?P<gallery>.+)/thumb/(?P<filename>.+)$', 'gal.views.view_thumbnail', name='view_thumbnail'),
-    url(r'^(?P<gallery>.+)/(?P<filename>.+)$', 'gal.views.image', name='image'),
-    url(r'^(?P<gallery>.+)$', 'gal.views.index'),
+    url(r'^(?P<gallery>[^/]+)/download$', 'gal.views.download_all'),
+    url(r'^(?P<gallery>[^/]+)/image/(?P<filename>.+)$', 'gal.views.view_image', name='view_image'),
+    url(r'^(?P<gallery>[^/]+)/thumb/(?P<filename>.+)$', 'gal.views.view_thumbnail', name='view_thumbnail'),
+    url(r'^(?P<gallery>[^/]+)/(?P<filename>.+)$', 'gal.views.image', name='image'),
+    url(r'^(?P<gallery>[^/]+)$', 'gal.views.index'),
 )
