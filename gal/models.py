5f77fd98d55a6aa71331c1ca5088b5f421b30ab8
Zoran Zaric <zz@zoranzaric.de>
2013-03-19 23:28:01 +0100

    Add image url property to gallery

--- gal/models.py
+++ gal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Gallery(models.Model):
     name = models.CharField(max_length=255, unique=True, default="")
@@ -10,6 +11,11 @@ class Image(models.Model):
     filename = models.CharField(max_length=255)
     gallery = models.ForeignKey(Gallery, null=True)
     picked = models.BooleanField(default=False)
+
+    @property
+    def image_url(self):
+        return reverse('gal.views.view_image',
+                       args=[self.gallery.name, self.filename])
 
     def __unicode__(self):
         return "%s/%s" % (self.gallery, self.filename)
